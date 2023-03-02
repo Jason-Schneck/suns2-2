124f8d130533dc1c59f91de518a4c28efddd3a4c
Jason-Schneck <jschnecky03@gmail.com>
2023-03-02 17:45:01 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let btn1 = document.querySelector('#btn-1');
+let btn2 = document.querySelector('#btn-2');
+let btn3 = document.querySelector('#btn-3');
+
+btn1.addEventListener('click', () =>{
+    document.body.style.backgroundImage = "url('valley-1.webp')"
+    document.body.style.color = "purple"
+})
+btn2.addEventListener('click', () =>{
+    document.body.style.backgroundImage = "url('eaglesStadium.webp')"
+    document.body.style.color = "white"
+})
+btn3.addEventListener('click', () =>{
+    document.body.style.backgroundImage = "url('philliesS.webp')"
+    document.body.style.color = "white"
+})
